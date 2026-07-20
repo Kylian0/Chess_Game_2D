ab18705c310850bb5bc87a8fac8eb1345a4b8975
Kylian <157616732+Kylian0@users.noreply.github.com>
2026-07-20 22:09:30 +0200

    Ajout de la création de l'échiquier ainsi que des labels

--- Assets/Scripts/Board/ChessBoard.cs
+++ Assets/Scripts/Board/ChessBoard.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
+using TMPro;
 
 public class ChessBoard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject casePrefab;
+    public TMP_Text textPrefab;
+
+    char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
     void Start()
     {
-        
+        CreateChessBoard();
+        CreateChessBoardLabel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateChessBoard()
     {
-        
+        // Créer un plateau d'échecs de 8x8 cases
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                float positionX = i - 3.5f;
+                float positionY = j - 3.5f;
+
+                GameObject caseInstance = Instantiate(casePrefab, new Vector3(positionX, positionY, 0), Quaternion.identity, parent: transform);
+                
+
+                SpriteRenderer spriteRenderer = caseInstance.GetComponent<SpriteRenderer>();
+                
+
+                if ((i + j) % 2 == 0)
+                {
+                    spriteRenderer.color = Color.black;
+                }
+                else
+                {
+                    spriteRenderer.color = Color.white;
+                }
+            }
+        }
+    }
+
+    public void CreateChessBoardLabel()
+    {
+        // Créer les lettres A à H en dessous du plateau
+        for (int i = 0; i < 8; i++)
+        {
+            TMP_Text letterLabel = Instantiate(textPrefab, transform);
+
+            letterLabel.transform.localPosition = new Vector3(i - 3.5f, -4.5f, 0);
+
+            letterLabel.text = letters[i].ToString();
+
+
+            // Créer les chiffres 1 à 8 à gauche du plateau
+            TMP_Text numberLabel = Instantiate(textPrefab, transform);
+
+            numberLabel.transform.localPosition = new Vector3(-4.5f, i - 3.5f, 0);
+
+            numberLabel.text = (i + 1).ToString();
+        }
     }
 }
